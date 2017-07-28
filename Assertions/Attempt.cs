cb1eed68c45a8cd827bb7595322b223b3580c7e1
Eric Lobdell <eric@ericlobdell.me>
2017-07-27 21:02:27 -0400

    added attemp class

---
+++ Assertions/Attempt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Xunit
+{
+  public static class Attempt
+  {
+    public static void ForMilliseconds(long timoutInMilliseconds, Action assertion)
+    {
+      Stopwatch timer = new Stopwatch();
+      timer.Start();
+
+      while (timer.ElapsedMilliseconds <= timoutInMilliseconds)
+      {
+        try
+        {
+          assertion();
+          break;
+        }
+        catch (Exception){}
+      }
+    }
+
+    public static void ForSeconds(int timoutInSeconds, Action assertion) =>
+      ForMilliseconds(timoutInSeconds * 1000, assertion);
+  }
+}
---
+++ Tests/AttemptTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Tests
+{
+  public class AttemptTests
+  {
+    [Fact]
+    public void ForMilliseconds_returns_before_timeout_when_test_passes()
+    {
+      var timeout = 3000;
+      var timer = new Stopwatch();
+
+      timer.Start();
+      Attempt.ForMilliseconds(timeout, () => Assert.True(true));
+
+      Assert.True(timer.ElapsedMilliseconds < timeout);
+    }
+
+    [Fact]
+    public void ForMilliseconds_returns_after_timeout_when_test_fails()
+    {
+      var timeout = 3000;
+      var timer = new Stopwatch();
+
+      timer.Start();
+      Attempt.ForMilliseconds(timeout, () => Assert.True(false));
+
+      Assert.True(timer.ElapsedMilliseconds >= timeout);
+    }
+
+    [Fact]
+    public void ForSeconds_returns_before_timeout_when_test_passes()
+    {
+      var timeout = 3;
+      var timer = new Stopwatch();
+
+      timer.Start();
+      Attempt.ForSeconds(timeout, () => Assert.True(true));
+
+      Assert.True(timer.Elapsed.Seconds < timeout);
+    }
+
+    [Fact]
+    public void ForSeconds_returns_after_timeout_when_test_fails()
+    {
+      var timeout = 3;
+      var timer = new Stopwatch();
+
+      timer.Start();
+      Attempt.ForSeconds(timeout, () => Assert.True(false));
+
+      Assert.True(timer.Elapsed.Seconds >= timeout);
+    }
+  }
+}
